2c6432e29b3d6d7a7afd1de10220b202a71cc0b1
Alex Bro <trans2007formers@gmail.com>
2026-04-19 23:35:31 +0300

    step9 finale

---
+++ __tests__/brain-even.test.js
@@ -0,0 +1,14 @@
+const isEven = require('./brain-even'); // предполагаемая функция
+
+
+describe('Brain Even Game', () => {
+  test('should return true for even numbers', () => {
+    expect(isEven(2)).toBe(true);
+    expect(isEven(4)).toBe(true);
+  });
+
+  test('should return false for odd numbers', () => {
+    expect(isEven(1)).toBe(false);
+    expect(isEven(3)).toBe(false);
+  });
+});
