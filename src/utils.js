0c33065ede7fcd707d876459d8f62df8f572980b
Nikita <crazyshot411@gmail.com>
2026-05-25 10:40:10 +0300

    nice

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,1 @@
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-export const isEven = (num) => num % 2 === 0;
+export default (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
